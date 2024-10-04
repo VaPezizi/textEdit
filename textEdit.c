19ba3dfea98571908cbaac63f4ad9cfd3ba233ce
Samuli Pihlaja <samuli.pihlaja.SP@gmail.com>
2024-10-04 17:00:21 +0300

    Added simple writing with a simple font

--- textEdit.c
+++ textEdit.c
@@ -4,12 +4,52 @@
 
 #define HEIGHT 600
 #define WIDTH 800
-
+#define BUFFERSIZE 500
 
 int main(){
-	
+	//Allocating memory for a Buffer of 500 characters
+	char * BUFFER = (char*) malloc(sizeof(char) * (BUFFERSIZE + 1));
+	BUFFER[BUFFERSIZE + 1] = '\0';
+
+	InitWindow(WIDTH, HEIGHT, "Text Edit");
+	SetTargetFPS(60);
+
+	int letterCount = 0;
+
 	while(!WindowShouldClose()){
-		continue;
+		
+		if(CheckCollisionPointRec(GetMousePosition(), (Rectangle) {0, 0, GetScreenWidth(), GetScreenHeight()})){
+			SetMouseCursor(MOUSE_CURSOR_IBEAM);
+			
+			int key = GetCharPressed();
+	
+			while(key > 0){
+				if((key>= 32) && (key <= 255) && (letterCount < BUFFERSIZE)){
+					
+					BUFFER[letterCount] = (char)key;
+					BUFFER[letterCount + 1] = '\0';
+					letterCount++;
+				}
+				
+				key = GetCharPressed();
+			}
+			if(IsKeyPressed(KEY_BACKSPACE)){
+				letterCount--;
+				if(letterCount < 0) letterCount = 0;
+				BUFFER[letterCount] = '\0';
+			}
+		}
+		
+		else SetMouseCursor(MOUSE_CURSOR_DEFAULT);
+	
+
+		BeginDrawing();
+		ClearBackground(WHITE);	
+		
+		DrawText(BUFFER, 10, 10, 40, MAROON);	
+		
+		EndDrawing();
+		printf("Arvo :%c \n", BUFFER[letterCount - 1]);
 	}	
 
 
